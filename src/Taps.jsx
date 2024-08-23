09d0e00cf31f2bea1472b04d5295fdb8d492110d
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-08-22 23:44:18 -0700

    Create state activetab and add onclick to buttons

--- src/Taps.jsx
+++ src/Taps.jsx
@@ -3,13 +3,18 @@ import { useState } from "react";
 function Taps() {
     const [activeTab, setActiveTab] = useState(localStorage.getItem("activeTab")||'tab1');
 
+    const tapHandler = (tab) => {
+        setActiveTab(tab);
+        localStorage.setItem("activeTab", tab);
+      };
+
   return (
     <div className="tabsContainer">
         <div className="buttons">
-            <button>Tab 1</button>
-            <button>Tab 2</button>
-            <button>Tab 3</button>
-            <button>Tab 4</button>
+            <button onClick={()=>tapHandler('tab1')}>Tab 1</button>
+            <button onClick={()=>tapHandler('tab2')}>Tab 2</button>
+            <button onClick={()=>tapHandler('tab3')}>Tab 3</button>
+            <button onClick={()=>tapHandler('tab4')}>Tab 4</button>
 
         </div>
         <div >
