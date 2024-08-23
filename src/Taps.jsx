44b777482132f488d0021a6d51d0009e29e37753
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-08-22 23:38:02 -0700

    the buttons was added to Taps.jsx

--- src/Taps.jsx
+++ src/Taps.jsx
@@ -2,9 +2,19 @@ import { useState } from "react";
 
 function Taps() {
     const [activeTab, setActiveTab] = useState(localStorage.getItem("activeTab")||'tab1');
-    
+
   return (
-    <div>
+    <div className="tabsContainer">
+        <div className="buttons">
+            <button>Tab 1</button>
+            <button>Tab 2</button>
+            <button>Tab 3</button>
+            <button>Tab 4</button>
+
+        </div>
+        <div >
+
+        </div>
       
     </div>
   )
